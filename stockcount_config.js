5ecff7eb574a73b85d8d63995aca16ff2d40de92
S-Kittiwat <68015483+S-Kittiwat@users.noreply.github.com>
2026-09-11 13:32:44 +0700

    Update stockcount_config.js

--- stockcount_config.js
+++ stockcount_config.js
@@ -2,7 +2,7 @@
 //  Stock Count App — Global Config
 // ============================================================
 var STOCKCOUNT_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxHqLjzYwlCb6exsOf1xgqUmpmwv6jfX4hgcnk3uTtzIzNZD4kWlqAoe3YdOpyiyzDZug/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwKsFyegooL4840yD2jzEfUFuT9Xix0HlXDcNuazNSLsQet9OoYtm0TWD5bvu0zOVSs/exec',
   APP_NAME: 'StockCount',
   VERSION: '1.0.1'
 };
